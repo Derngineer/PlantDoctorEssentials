55470fabfc88b09fad76928f9bdfcaebafcd4f11
Derby <dmatderby@gmail.com>
2023-10-15 00:54:26 +0400

    video changes from .mov to mp4

--- doctor-frontend/src/components/Video.tsx
+++ doctor-frontend/src/components/Video.tsx
@@ -7,8 +7,9 @@ const Video =()=>{
         <Fragment>
         <div>
         <motion.div whileHover={{scale: 1.1}} whileTap={{scale: 0.9}}>
-        <video src="/Screen Recording 2023-09-03 at 6.47.18 AM.mov" autoPlay
+        <video src="/Screen Recording 2023-09-03 at 6.47.18 AM.mp4" autoPlay
         muted
+        controls
         loop  style={{height:"100%",width:"100%", objectFit:"cover"}} />
         </motion.div>
         </div>
