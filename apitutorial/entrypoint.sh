e5597a2bb38dd14287e560f882c05acad2472a78
Derby <dmatderby@gmail.com>
2023-10-26 02:37:55 +0400

    adjust .sh file

--- apitutorial/entrypoint.sh
+++ apitutorial/entrypoint.sh
@@ -5,4 +5,6 @@ python manage.py collectstatic --no-input
 
 DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
 
-gunicorn investment_manager.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn apitutorial.wsgi:application --bind 0.0.0.0:$PORT
+
+
