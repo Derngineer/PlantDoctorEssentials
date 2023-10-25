7cc1f5e879a59ec992cbb3c04792afebce5157b6
Derby <dmatderby@gmail.com>
2023-10-26 03:53:27 +0400

    reordering static files

--- apitutorial/apitutorial/settings.py
+++ apitutorial/apitutorial/settings.py
@@ -125,12 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 # settings.py
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_URL = '/static/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/static/'
 
 
 
